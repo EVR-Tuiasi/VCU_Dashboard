b475da66f73cf9ce724d2b3cfc5ff617da3d2189
ConduracheGF <condurachegabriel14@gmail.com>
2026-02-20 20:39:13 +0200

    modificari de baza

--- code/K344_Dashboard/src/Segments/AS1115.c
+++ code/K344_Dashboard/src/Segments/AS1115.c
@@ -20,7 +20,21 @@ extern "C" {
 /*==================================================================================================
 *                                       LOCAL MACROS
 ==================================================================================================*/
+//adrese pentru LPI2C0 pentru S32K344
+#define LPI2C0_BASE          (0x40440000UL)									//Base Address
+#define LPI2C_MCR            (*(volatile uint32_t*)(LPI2C0_BASE + 0x10))	//Control Register
+#define LPI2C_MSR            (*(volatile uint32_t*)(LPI2C0_BASE + 0x14))	//Status Register
+#define LPI2C_MTDR           (*(volatile uint32_t*)(LPI2C0_BASE + 0x1C))	//Transmit Data
+#define LPI2C_MRDR           (*(volatile uint32_t*)(LPI2C0_BASE + 0x20))	//Receive Data
 
+//adresa driver de trimitere
+#define AS1115_I2C_ADDR      0x00
+
+//comenzi pentru LPI2C MTDR
+#define CMD_START            (0x0400U)
+#define CMD_STOP             (0x0200U)
+#define CMD_TRANSMIT         (0x0000U)
+#define CMD_RECEIVE          (0x0100U)
 
 /*==================================================================================================
 *                                      LOCAL CONSTANTS
@@ -50,18 +64,69 @@ extern "C" {
 /*==================================================================================================
 *                                       LOCAL FUNCTIONS
 ==================================================================================================*/
-
+static void LPI2C_Wait(){
+	//se asteapta pana cand FIFO de transmitere a datelor are loc
+	while(((*(volatile uint32_t*)(LPI2C0_BASE + 0x18)) & 0xFF) > 2);
+}
 
 /*==================================================================================================
 *                                       GLOBAL FUNCTIONS
 ==================================================================================================*/
 
 void AS1115_Write(AS1115Registers_t SelectedRegister, uint8_t Value){
-    /*TODO*/
+    //START + adresa pe write
+	LPI2C_Wait();
+	LPI2C_MTDR = CMD_START | (AS1115_I2C_ADDR << 1);
+
+	//trimite adresa de inregistrare
+	LPI2C_Wait();
+	LPI2C_MTDR = CMD_START | (uint8_t)SelectedRegister;
+
+	//trimitere date
+	LPI2C_Wait();
+	LPI2C_MTDR = CMD_TRANSMIT | Value;
+
+	//STOP
+	LPI2C_Wait();
+	LPI2C_MTDR = CMD_STOP;
 }
 
 uint8_t AS1115_Read(AS1115Registers_t SelectedRegister){
-    /*TODO*/
+    uint32_t data = 0;
+	//START + adresa pe write
+	LPI2C_Wait();
+	LPI2C_MTDR = CMD_START | (AS1115_I2C_ADDR << 1);
+
+	//trimite adresa de inregistrare
+	LPI2C_Wait();
+	LPI2C_MTDR = CMD_START | (uint8_t)SelectedRegister;
+
+	//REPEATED START + adresa pe read (bitul LSB setat pe 1)
+	LPI2C_Wait();
+	LPI2C_MTDR = CMD_START | (AS1115_I2C_ADDR << 1 | 0x01);
+
+	//comanda de receive (0x00 inseamna primirea 1 octet)
+	LPI2C_Wait();
+	LPI2C_MTDR = CMD_RECEIVE | 0x00;
+
+	//STOP
+	LPI2C_Wait();
+	LPI2C_MTDR = CMD_STOP;
+
+	//asteapta pana cand receive FIFO are date disponibile
+	//verificam registrul MSR sau MRSR
+	//in LPI2C0_BASE + 0x24 avem MRSR
+	while(((*(volatile uint32_t*)(LPI2C0_BASE + 0x24)) & 0xFF) == 0);
+
+	//citirea datelor
+	receivedValue = LPI2C_MRDR;
+
+	//verificam bitul de RX Empty pentru a ne asigura ca datele sunt valide
+	if (!(data & (1 << 14))) {
+		return (uint8_t)(data & 0xFF);
+	}
+
+	return 0;
 }
 
 #ifdef __cplusplus
--- code/K344_Dashboard/src/Segments/SevenSegments.c
+++ code/K344_Dashboard/src/Segments/SevenSegments.c
@@ -12,11 +12,16 @@ extern "C" {
 
 #include "stdint.h"
 #include "SevenSegments.h"
+#include "AS1115.h"
 
 /*==================================================================================================
 *                          LOCAL TYPEDEFS (STRUCTURES, UNIONS, ENUMS)
 ==================================================================================================*/
-
+typedef struct {
+	int16_t SPEED_KMH;
+	int16_t BATTERY_PERCENTAGE;
+	int16_t TEMPERATURE;
+} SegmentsValues_t;
 
 /*==================================================================================================
 *                                       LOCAL MACROS
@@ -31,7 +36,13 @@ extern "C" {
 /*==================================================================================================
 *                                      LOCAL VARIABLES
 ==================================================================================================*/
+//instantierea structurii pentru memorarea valorilor
+static SegmentsValues_t SegmentsValues = {0, 0, 0};
 
+//mapare pentru fiecare grup: gr 0 = temperatura (dig0-dig1), gr 1 = speed (dig2-dig4), gr 2 = battery (dig5-dig7)
+static const uint8_t GroupTemp[] = {DIGIT0, DIGIT1}; 				// 2 segmente
+static const uint8_t GroupSpeed[] = {DIGIT2, DIGIT3, DIGIT4}; 		// 3 segmente
+static const uint8_t GroupBattery[] = {DIGIT5, DIGIT6, DIGIT7}; 	// 3 segmente
 
 /*==================================================================================================
 *                                      GLOBAL CONSTANTS
@@ -51,6 +62,18 @@ extern "C" {
 /*==================================================================================================
 *                                       LOCAL FUNCTIONS
 ==================================================================================================*/
+//functie auxiliara pentru scrierea unui numar pe un grup de digituri
+static void DisplayWriteNumber(const uint8_t* GroupDigits, uint8_t size, int16_t value) {
+	for(uint8_t i = 0; i < size; i++){
+		//daca valoarea devine 0 stingem restul, apelam 0x0F
+		if(value == 0 && i > 0){
+			AS1115_Write((AS1115Registers_t)GroupDigits[i], 0x0F); //scriem BLANK
+		} else {
+			AS1115_Write((AS1115Registers_t)GroupDigits[i], (uint8_t)(value%10));
+			value /= 10;
+		}
+	}
+}
 
 
 /*==================================================================================================
@@ -58,19 +81,39 @@ extern "C" {
 ==================================================================================================*/
 
 void Segments_Init(void){
-    /*TODO implementation */
+    /*initializare drive*/
+	AS1115_Write(SHUTDOWN, 0x01);			//iesire din shutdown
+	AS1115_Write(SCAN_LIMIT, 0x07);			//activeaza digitii 0-7
+	AS1115_Write(DECODE_MODE, 0xFF);		//decodificare BCD pe digitii 0-7
+	AS1115_Write(GLOBAL_INTENSITY, 0x08);	//intensitatea setata
 }
 
 void Segments_Test(void){
-    /*TODO implementation */
+    //aprinde toate segmentele pentru testare
+	AS1115_Write(DISPLAY_TEST_MODE, 0x01);
 }
 
 void Segments_Set(SegmentsMonitoredValue_t SelectedMonitor, int16_t Value){
-    /*TODO implementation */
+    /*se seteaza in functie de valoarea monitorizata selectata*/
+	switch(SelectedMonitor){
+		case SPEED_KMH: 			SegmentsValues.SPEED_KMH = Value;
+									break;
+		case BATTERY_PERCENTAGE: 	SegmentsValues.BATTERY_PERCENTAGE = Value;
+									break;
+		case TEMPERATURE: 			SegmentsValues.TEMPERATURE = Value;
+						  	  	  	break;
+		default:					break;
+
+	}
 }
 
 void Segments_Update(void){
-    /*TODO implementation */    
+    //afisare temperatura pe grupul de 2 segmente
+	DisplayWriteNumber(GroupTemp, 2, SegmentsValues.TEMPERATURE);
+	//afisare viteza pe grupul de 3 segmente
+	DisplayWriteNumber(GroupSpeed, 3 SegmentsValues.SPEED_KMH);
+	//afisare baterie pe grupul de 3 segmente
+	DisplayWriteNumber(GroupBattery, 3, SegmentsValues.BATTERY_PERCENTAGE);
 }
 
 #ifdef __cplusplus
