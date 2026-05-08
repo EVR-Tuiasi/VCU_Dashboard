4c0e6c83b150c09dd2e19f451094c4ded3afe266
ConduracheGF <condurachegabriel14@gmail.com>
2026-05-08 22:47:03 +0300

    New Project

---
+++ code/VCU_Dashboard_K344/src/Segments/AS1115.c
@@ -0,0 +1,117 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/*==================================================================================================
+*                                        INCLUDE FILES
+* 1) system and project includes
+* 2) needed interfaces from external units
+* 3) internal and external interfaces from this unit
+==================================================================================================*/
+
+#include "Segments/AS1115.h"
+#include "CDD_I2c.h"
+
+/*==================================================================================================
+*                          LOCAL TYPEDEFS (STRUCTURES, UNIONS, ENUMS)
+==================================================================================================*/
+
+
+/*==================================================================================================
+*                                       LOCAL MACROS
+==================================================================================================*/
+//adresa slave a driverului
+#define DRIVER_SLAVE_ADDRESS 0x00
+//canalul folosit
+#define I2C_USED_CHANNEL 0
+/*==================================================================================================
+*                                      LOCAL CONSTANTS
+==================================================================================================*/
+
+
+/*==================================================================================================
+*                                      LOCAL VARIABLES
+==================================================================================================*/
+
+
+/*==================================================================================================
+*                                      GLOBAL CONSTANTS
+==================================================================================================*/
+
+
+/*==================================================================================================
+*                                      GLOBAL VARIABLES
+==================================================================================================*/
+//cerere globala
+I2c_RequestType request;
+/*==================================================================================================
+*                                   LOCAL FUNCTION PROTOTYPES
+==================================================================================================*/
+
+
+/*==================================================================================================
+*                                       LOCAL FUNCTIONS
+==================================================================================================*/
+
+
+/*==================================================================================================
+*                                       GLOBAL FUNCTIONS
+==================================================================================================*/
+
+void AS1115_Write(AS1115Registers_t SelectedRegister, uint8_t Value){
+    //structura bufferului
+	uint8_t buffer[2] = {
+    		(uint8_t)SelectedRegister,
+			Value
+    };
+
+    //pregatire cerere
+    request.SlaveAddress = DRIVER_SLAVE_ADDRESS;
+    request.BitsSlaveAddressSize = false;
+    request.HighSpeedMode = false;
+    request.ExpectNack = false;
+    request.RepeatedStart = false;
+    request.BufferSize = 2;
+    request.DataDirection = I2C_SEND_DATA;
+    request.DataBuffer = buffer;
+
+    I2c_SyncTransmit(I2C_USED_CHANNEL, &request); //cerere pe canalul 0
+}
+
+uint8_t AS1115_Read(AS1115Registers_t SelectedRegister){
+    // ---!!!--- S-a incercat introducerea unei referinte la SelectedRegister in writeRequest.DataBuffer = &(registru);
+	// ---S-au facut 3 erori la compile, rezolvarea a constat in revenirea la varianta anterioara
+
+	uint8_t value = 0;
+
+    //scriem registrul dorit
+    request.SlaveAddress = DRIVER_SLAVE_ADDRESS;
+    request.BitsSlaveAddressSize = false;
+    request.HighSpeedMode = false;
+    request.ExpectNack = false;
+    request.RepeatedStart = false;
+    request.BufferSize = 1;
+    request.DataDirection = I2C_SEND_DATA;
+    request.DataBuffer = (uint8_t*)&SelectedRegister;
+
+    I2c_SyncTransmit(I2C_USED_CHANNEL, &request);
+
+    //citim valoarea
+    request.SlaveAddress = DRIVER_SLAVE_ADDRESS;
+    request.BitsSlaveAddressSize = false;
+    request.HighSpeedMode = false;
+    request.ExpectNack = false;
+    request.RepeatedStart = false;
+    request.BufferSize = 1;
+    request.DataDirection = I2C_RECEIVE_DATA;
+    request.DataBuffer = &value;
+
+    I2c_SyncTransmit(I2C_USED_CHANNEL, &request);
+
+    return value;
+}
+
+#ifdef __cplusplus
+}
+#endif
---
+++ code/VCU_Dashboard_K344/src/Segments/SevenSegments.c
@@ -0,0 +1,227 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/*==================================================================================================
+*                                        INCLUDE FILES
+* 1) system and project includes
+* 2) needed interfaces from external units
+* 3) internal and external interfaces from this unit
+==================================================================================================*/
+
+#include "stdint.h"
+#include "CDD_I2c.h"
+#include "Segments/SevenSegments.h"
+#include "Segments/AS1115.h"
+
+/*==================================================================================================
+*                          LOCAL TYPEDEFS (STRUCTURES, UNIONS, ENUMS)
+==================================================================================================*/
+
+
+/*==================================================================================================
+*                                       LOCAL MACROS
+==================================================================================================*/
+
+
+/*==================================================================================================
+*                                      LOCAL CONSTANTS
+==================================================================================================*/
+static const SegmentsGroups_t SegmentsGroups = {
+	{//DigitGroup_Speed
+		DIGIT_0,
+		DIGIT_1,
+		DIGIT_2
+	},
+	{//DigitGroup_Battery
+		DIGIT_3,
+		DIGIT_4,
+		DIGIT_5
+	},
+	{//DigitGroup_Temperature
+		DIGIT_6,
+		DIGIT_7
+	}
+};
+
+/*==================================================================================================
+*                                      LOCAL VARIABLES
+==================================================================================================*/
+// -- SPEED_KMH pentru digiti 0-2
+// -- BATTERY_PERCENTAGE pentru digiti 3-5
+// -- TEMPERATURE pentru digiti 6-7
+static uint8_t displayBuffer[8] = {0};
+
+//
+/*==================================================================================================
+*                                      GLOBAL CONSTANTS
+==================================================================================================*/
+
+
+/*==================================================================================================
+*                                      GLOBAL VARIABLES
+==================================================================================================*/
+
+
+/*==================================================================================================
+*                                   LOCAL FUNCTION PROTOTYPES
+==================================================================================================*/
+
+
+/*==================================================================================================
+*                                       LOCAL FUNCTIONS
+==================================================================================================*/
+
+
+/*==================================================================================================
+*                                       GLOBAL FUNCTIONS
+==================================================================================================*/
+
+void Segments_Init(void){
+
+	// -- Seteaza modul Shutdown cu Reset Feature Register
+	AS1115_Write(SHUTDOWN, 0x00);
+
+	// -- Seteaza Luminozitatea Globala la 7 Segment Display-uri
+	AS1115_Write(GLOBAL_INTENSITY, 0x0F);
+
+	// -- Schimba Feature Register pentru modul de decodificare al 7 Segment Display
+	AS1115_Write(FEATURE, 0x00);
+
+	// -- Seteaza ca toate Segmentele de pe display sa fie stinse
+	for(uint8_t i = 0; i < 8; i++){
+		//pentru teste poti pune 4
+		AS1115_Write((AS1115Registers_t)(DIGIT0 + i), 0x0F);
+	}
+
+	// -- Seteaza cati pini folosim de la dig0 pana la dig7 [ex: 0x00 - dig0 | 0x03 - dig0 -> dig3]
+	AS1115_Write(SCAN_LIMIT, 0x07);
+	//aici era 3 pentru teste pe PCB
+
+	// -- Seteaza pana la ce pin folosim decodificare pe digits [ex: 0x03 - 00000011 - Decodifica pe dig0 si dig1, ne luam dupa pozitia bitilor de la LSB la MSB]
+	AS1115_Write(DECODE_MODE, 0xFF);
+
+	// -- Seteaza Normal Mode fara modificari la Feature Register
+	AS1115_Write(SHUTDOWN, 0x81);
+}
+
+void Segments_Test(void){
+	//variabila de test pentru simularea vitezei
+	static uint16_t test_speed = 0;
+	//variabila de test pentru simularea bateriei
+	static uint16_t test_battery = 110;
+	//variabila de test pentru simularea temperaturii
+	static uint16_t test_temperature = 0;
+	//counter pentru controlul refresh
+	static uint32_t loop_counter = 0;
+	//pas de scadere al bateriei
+	static uint16_t step = 1;
+
+	while(1){
+		// -- Actualizam valorile de test
+		test_speed = (test_speed + 1) % 10000; //viteza urca pana la 999 km/h
+		test_temperature = (loop_counter % 1000); //temperatura variaza intre 20-60 grade
+
+		// -- La fiecare 10 cicluri, consumam o unitate din baterie
+		if(loop_counter % 10 == 0){
+			if(test_battery > 0){
+				test_battery -= 1;
+			} else {
+				test_battery = 1000;
+			}
+		}
+
+		// -- Mapam pe grupurile de digituri valorile de test
+		//Segments_Set(SPEED_KMH, test_speed);
+		//Segments_Set(BATTERY_PERCENTAGE, test_battery);
+		Segments_Set(TEMPERATURE, test_temperature);
+
+
+		// -- Trimitem tot bufferul catre driverul AS1115 prin I2C
+		Segments_Update();
+
+		// -- Controlul vitezei de refresh
+		for(volatile uint32_t delay = 0; delay < 50000UL; delay++);
+	    loop_counter++;
+	}
+}
+
+void Segments_Set(SegmentsMonitoredValue_t SelectedMonitor, uint16_t Value){
+	// -- SPEED_KMH pentru digiti 0-2
+	// -- BATTERY_PERCENTAGE pentru digiti 3-5
+	// -- TEMPERATURE pentru digiti 6-7
+
+	switch(SelectedMonitor) {
+		case SPEED_KMH:
+			//limite de 0-999 kmh
+			if(Value < 0) Value = 0;
+			if(Value > 9999) Value = 9999;
+
+			//valoarea va iesi de forma XYZ
+			displayBuffer[SegmentsGroups.DigitGroup_Speed[0]] = (uint8_t)((Value / 10) % 10);
+			displayBuffer[SegmentsGroups.DigitGroup_Speed[1]] = (uint8_t)((Value / 100) % 10);
+			displayBuffer[SegmentsGroups.DigitGroup_Speed[2]] = (uint8_t)((Value / 1000) % 10);
+
+			//stingem secventa de digituri din fata egale cu 0
+			if(((uint8_t)((Value / 1000) % 10)) == 0){
+				displayBuffer[SegmentsGroups.DigitGroup_Speed[2]] |= 0x0F;
+				if(((uint8_t)((Value / 100) % 10)) == 0){
+					displayBuffer[SegmentsGroups.DigitGroup_Speed[1]] |= 0x0F;
+				}
+			}
+			break;
+		case BATTERY_PERCENTAGE:
+		    //limite de 0.00-100%
+			if(Value < 0) Value = 0;
+		    if(Value > 1000) Value = 1000;
+
+		    if (Value == 1000) {
+		        //valoare baterie plina
+		        displayBuffer[SegmentsGroups.DigitGroup_Battery[0]] = 0;
+		        displayBuffer[SegmentsGroups.DigitGroup_Battery[1]] = 0;
+		        displayBuffer[SegmentsGroups.DigitGroup_Battery[2]] = 1;
+		    } else {
+		        //valori de forma xy.z
+		        displayBuffer[SegmentsGroups.DigitGroup_Battery[0]] = (uint8_t)(Value % 10);
+		        displayBuffer[SegmentsGroups.DigitGroup_Battery[1]] = ((uint8_t)(Value / 10) % 10) | 0x80;
+		        displayBuffer[SegmentsGroups.DigitGroup_Battery[2]] = (uint8_t)(Value / 100);
+
+		        //stingem digitul din fata
+		        if (((uint8_t)(Value / 100)) == 0){
+		        	displayBuffer[SegmentsGroups.DigitGroup_Battery[2]] |= 0x0F;
+		        }
+		    }
+		    break;
+		case TEMPERATURE:
+			//limite de 0-60 grade Celsius
+			if(Value < 0) Value = 0;
+			if(Value > 999) Value = 999;
+
+			//valoarea va iesi de forma XY
+			displayBuffer[SegmentsGroups.DigitGroup_Speed[0]] = (uint8_t)((Value / 10) % 10);
+			displayBuffer[SegmentsGroups.DigitGroup_Speed[1]] = (uint8_t)((Value / 100) % 10);
+
+			//stingem primul digit daca e 0
+			if(((uint8_t)((Value / 100) % 10)) == 0){
+				displayBuffer[SegmentsGroups.DigitGroup_Speed[1]] |= 0x0F;
+			}
+	        break;
+	    default:
+		    break;
+	}
+}
+
+void Segments_Update(void){
+    // -- Actualizeaza digitii din bufferul local
+	for(uint8_t i = 0; i < 8; i++){
+		//aici e 4 pentru teste
+		AS1115_Write((AS1115Registers_t)(DIGIT0 + i), displayBuffer[i]);
+	}
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+/** @} */
